cd48c27e07ee4b65f54ddb8044b124b2e6f3c84a
Chris Sprehe <chris.sprehe@gmail.com>
2014-03-22 14:15:17 -0500

    pushed up failing test to show the centers are different

--- spec/models/geo_hash_converter_spec.rb
+++ spec/models/geo_hash_converter_spec.rb
@@ -22,4 +22,24 @@ describe GeoHashConverter do
       ])
     end
   end
+
+  describe "#same_center?" do
+    let(:bounding_box1) { GeoHash.decode_bbox("dr5ru49f") }
+    let(:bounding_box2) { GeoHash.decode_bbox("dr5ru4") }
+    let(:bounding_box3) { GeoHash.decode_bbox("dr5r") }
+    let(:bounding_box4) { GeoHash.decode_bbox("dr") }
+    let(:boxes) { [bounding_box1, bounding_box2, bounding_box3, bounding_box4] }
+
+    it "returns the same center point if same geo hash string but less precise" do
+      center_points = Set.new
+      boxes.each do |box_array|
+        point1 = RGeo::Geographic.simple_mercator_factory.point(box_array.first.last, box_array.first.first)
+        point2 = RGeo::Geographic.simple_mercator_factory.point(box_array.last.last, box_array.last.first)
+        bounding_box = RGeo::Cartesian::BoundingBox.create_from_points(point1, point2)
+        center_points.add([bounding_box.center_x, bounding_box.center_y])
+      end
+      puts center_points.inspect
+      expect(center_points.size).to eq(1)
+    end
+  end
 end
